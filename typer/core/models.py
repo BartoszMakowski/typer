928727a02b0096246bb58baa549d7f71b5815ee6
BartoszMakowski <baboshpl@gmail.com>
2017-10-15 01:33:42 +0200

    fixed models + made migrations

--- typer/core/models.py
+++ typer/core/models.py
@@ -10,17 +10,6 @@ class Wallet(models.Model):
     creation_time = models.DateTimeField(default=datetime.now)
 
 
-class Bet(models.Model):
-    wallet = models.ForeignKey(Wallet, null=False)
-    event = models.ForeignKey(Event, null=False)
-    value = models.FloatField(blank=False)
-    odd = models.FloatField(blank=False)
-    reward = models.FloatField(blank=False)
-    creation_time = models.DateTimeField(default=datetime.now, blank=False)
-    open = models.BooleanField(default=True, blank=False)
-    won = models.BooleanField(blank=True)
-
-
 class Event(models.Model):
     author = models.ForeignKey(User, null=False)
     start_time = models.DateTimeField(blank=False)
@@ -30,8 +19,19 @@ class Event(models.Model):
     description = models.TextField(blank=True)
     home_name = models.CharField(max_length=50, blank=False)
     away_name = models.CharField(max_length=50, blank=False)
-    home_odd = models.FloatField(min=1.0, blank=False)
-    away_odd = models.FloatField(min=1.0, blank=False)
-    draw_odd = models.FloatField(min=1.0, blank=False)
+    home_odd = models.FloatField(default=1.0, blank=False)
+    away_odd = models.FloatField(default=1.0, blank=False)
+    draw_odd = models.FloatField(default=1.0, blank=False)
     closed = models.BooleanField(default=False, blank=False)
     result = models.IntegerField(blank=True)
+
+
+class Bet(models.Model):
+    wallet = models.ForeignKey(Wallet, null=False)
+    event = models.ForeignKey(Event, null=False)
+    value = models.FloatField(blank=False)
+    odd = models.FloatField(blank=False)
+    reward = models.FloatField(blank=False)
+    creation_time = models.DateTimeField(default=datetime.now, blank=False)
+    open = models.BooleanField(default=True, blank=False)
+    won = models.BooleanField(blank=True)
