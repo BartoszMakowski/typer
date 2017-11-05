e90a23c4061a4f94eb90ae47713979e3e2865db3
BartoszMakowski <baboshpl@gmail.com>
2017-11-05 18:17:34 +0100

    changed new event form + added jinja add_class filter

--- typer/jinja2.py
+++ typer/jinja2.py
@@ -2,16 +2,20 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
 from django.utils.timezone import template_localtime
 from jinja2 import Environment
+from widget_tweaks.templatetags import widget_tweaks
 
 
 def environment(**options):
     env = Environment(**options)
     env.filters.update({
         'localtime': template_localtime,
+        'add_class': widget_tweaks.add_class,
+        'set_attr': widget_tweaks.set_attr,
     })
     env.globals.update({
         'static': staticfiles_storage.url,
         'url': reverse,
         'localtime': template_localtime,
+        'render_field': widget_tweaks.render_field
     })
     return env
